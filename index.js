8d1929b784b5038114cd77487321c4590fba5f8e
rifatkaynas <rifatkaynas@gmail.com>
2018-03-13 03:50:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "Let's talk about space.";
+var topic = "space";
 
-var conversation = `Hello, ${specialguest}!, ${topic}`;
+var conversation = `Hello, ${specialGuest}!, Let/'s talk about${topic}`;
